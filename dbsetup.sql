2abdb5887d1e1d47299d8471139a4df5f9ac1cc6
shmyg <serge.shmygelskyy@gmail.com>
2015-06-22 18:24:21 +0300

    Initial version

---
+++ dbsetup.sql
@@ -0,0 +1,24 @@
+CREATE	TABLE all_data
+	(
+	order_comment	VARCHAR2(1000),
+	mark_campaign	VARCHAR2(200),
+	customer_status	VARCHAR2(200),
+	order_date	DATE,
+	customer_name	VARCHAR2(200),
+	customer_address	VARCHAR2(1000),
+	phone_number	VARCHAR2(30),
+	add_phone	VARCHAR2(30),
+	mark_campaign1	VARCHAR2(200),
+	cranberry	NUMBER,
+	cowberry	NUMBER,
+	blueberry	NUMBER,
+	order_amt	NUMBER,
+	payment_method	VARCHAR2(200),
+	order_status	VARCHAR2(200),
+	delivery	VARCHAR2(200),
+	consultant	VARCHAR2(200),
+	doctor		VARCHAR2(200),
+	diagnosis	VARCHAR2(1000),
+	order_comment1	VARCHAR2(1000)
+)
+/
